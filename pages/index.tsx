8856198f31a60049ea0e11e9fb3ec8e40899285c
Damian Huchin <huchin.h.damian@gmail.com>
2022-10-12 11:01:34 -0500

    Fix grammar error with fruits

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import { Box, Typography, Grid } from '@mui/material'
 
 import Header from '../components/Header'
-import FrutsCard from '../components/FrutsCard'
-import frutList from '../utils/frutsList.json'
+import FruitsCard from '../components/FruitsCard'
+import fruitList from '../utils/fruitsList.json'
 
 const Home = () => {
   return (
     <Box>
       <Header />
-      <Typography variant="h2">Fruts Shop</Typography>
+      <Typography variant="h2">Fruits Shop for you</Typography>
       <Grid container justifyContent="space-around" wrap="wrap">
-        {frutList.map(frut => (
-          <FrutsCard key={frut.name} frutData={frut} />
+        {fruitList.map(frut => (
+          <FruitsCard key={frut.name} frutData={frut} />
         ))}
       </Grid>
     </Box>
